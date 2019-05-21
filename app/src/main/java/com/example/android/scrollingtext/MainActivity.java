f51806cda0253f449de974d9fb0f4252157dfe96
BYS <pumapupa@gmail.com>
2019-05-21 16:12:12 +0800

    Test FullScreen, add toast change.

--- app/src/main/java/com/example/android/scrollingtext/MainActivity.java
+++ app/src/main/java/com/example/android/scrollingtext/MainActivity.java
@@ -1,23 +1,43 @@
 package com.example.android.scrollingtext;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Toast mToastMsg;
+    Toast curToast;
+    Toast nextToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            getSupportActionBar().hide();
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY );
+        }
         setContentView(R.layout.activity_main);
-        mToastMsg = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
-        mToastMsg.setGravity(Gravity.CENTER, 0, 0);
+
+        curToast = Toast.makeText(this, R.string.toast_message_1, Toast.LENGTH_SHORT);
+        nextToast = Toast.makeText(this, R.string.toast_message_2, Toast.LENGTH_SHORT);
     }
 
-    public void showToast(View view) {
-        mToastMsg.show();
+    public void showToast (View view) {
+        curToast.show();
+
+        Toast tmp = curToast;
+        curToast = nextToast;
+        nextToast = tmp;
     }
 }
