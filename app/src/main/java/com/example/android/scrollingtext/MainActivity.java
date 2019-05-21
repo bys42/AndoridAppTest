0083b9b886b4897b49fd81ccccaf82a40bc66c8e
BYS <pumapupa@gmail.com>
2019-05-21 11:28:24 +0800

    Code Challenge done

--- app/src/main/java/com/example/android/scrollingtext/MainActivity.java
+++ app/src/main/java/com/example/android/scrollingtext/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.android.scrollingtext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Toast mToastMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToastMsg = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        mToastMsg.setGravity(Gravity.CENTER, 0, 0);
+    }
+
+    public void showToast(View view) {
+        mToastMsg.show();
     }
 }
